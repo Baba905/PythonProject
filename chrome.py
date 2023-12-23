4b985ef6d7bfed65ae67615bed49cb5a7f396638
Baba905 <baba78122@gmail.com>
2023-12-23 12:55:53 +0100

    Tinder autoswaping bot, project not full completed

---
+++ chrome.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+
+# Create and configure driver
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(chrome_options)
+
+# Launch browser
+driver.get(url="https://tinder.com/app/recs")
+driver.implicitly_wait(10)
+
+
+email = "bdiop8683@gmail.com"
+password ="loveTKD99++"
+
+connexion = driver.find_element(By.LINK_TEXT, value="Connexion")
+connexion.click()
---
+++ main.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+driver = webdriver.Firefox()
+driver.get("https://tinder.com/")
+
+
+email = "bdiop8683@gmail.com"
+password ="loveTKD99++"
+
+connexion = driver.find_element(By.LINK_TEXT, value="Connexion")
+connexion.click()
+sleep(5)
+facebook = driver.find_element(By.CSS_SELECTOR, value="div.My\(12px\):nth-child(2) > button:nth-child(1)")
+facebook.click()
+# sleep(10)
+# cookies = driver.find_element(By.CSS_SELECTOR,value='button[title="Refuser les cookies optionnels"]')
+# cookies.click()
+
+sleep(10)
+
+email_input = driver.find_element(By.CSS_SELECTOR, value='input#email.inputtext._55r1')
+email_input.send_keys(email)
+password_input = driver.find_element(By.CSS_SELECTOR, value="#pass")
+connect_button = driver.find_element(By.CSS_SELECTOR, value="#u_0_0_P0")
+connect_button.click(Keys.ENTER)
